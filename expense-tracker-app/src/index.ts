6e72797fd2709b9c61c93bd1f4fdc4c25a7e86fe
hashfimw <hashfimawarid@Hashfis.local>
2024-11-26 09:37:50 +0700

    update

--- expense-tracker-app/src/index.ts
+++ expense-tracker-app/src/index.ts
@@ -1,14 +1,35 @@
 import express, { Request, Response } from "express";
 import { ExpenseRouter } from "./routers/expense.router";
+import "dotenv/config";
 import cors from "cors";
+import pool from "./config/db";
+import { ExpenseV2Router } from "./routers/expensev2.router";
 
 const app = express();
 const PORT = 4000;
 const expenseRouter = new ExpenseRouter();
+const expenseV2Router = new ExpenseV2Router();
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/expenses", expenseRouter.getRouter());
+app.use("/api/v2/expenses", expenseV2Router.getRouter());
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.log("Error acquiring client", err.stack);
+  }
+  if (client) {
+    client.query("SET search_path TO test", (queryErr) => {
+      if (queryErr) {
+        console.log("Error setting search path", queryErr.stack);
+      } else {
+        console.log('Success connection "test" 🦴');
+      }
+      release();
+    });
+  }
+});
 
 app.get("/api", (req: Request, res: Response) => {
   res.status(200).send("Hello, Welcome to my API");
---
+++ expense-tracker-app/src/routers/expensev2.router.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { ExpenseV2Controller } from "../controllers/expensev2.controller";
+
+export class ExpenseV2Router {
+  private router: Router;
+  private expensev2Controller: ExpenseV2Controller;
+
+  constructor() {
+    this.router = Router();
+    this.expensev2Controller = new ExpenseV2Controller();
+    this.intialiazeRouters();
+  }
+
+  private intialiazeRouters() {
+    this.router.get("/", this.expensev2Controller.getExpense);
+    this.router.post("/", this.expensev2Controller.addExpense);
+
+    this.router.get("/:id", this.expensev2Controller.getExpenseId);
+    this.router.patch("/:id", this.expensev2Controller.editExpense);
+    this.router.delete("/:id", this.expensev2Controller.deleteExpense);
+  }
+
+  getRouter(): Router {
+    return this.router;
+  }
+}
